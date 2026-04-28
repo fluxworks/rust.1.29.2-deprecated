0368a0ea9561fdb9b6acc0f18170d40eb3893724
fluxworks <mrjustinlucas@gmail.com>
2026-04-28 19:12:10 -0400

    26.04.28.1910
    librustc_mir/interpret/mod.rs

--- src/librustc/mir/interpret/mod.rs
+++ src/librustc/mir/interpret/mod.rs
@@ -7,6 +7,17 @@ macro_rules! err {
 
 mod error;
 mod value;
+mod cast;
+mod const_eval;
+mod eval_context;
+mod place;
+mod machine;
+mod memory;
+mod operator;
+mod step;
+mod terminator;
+mod traits;
+
 
 pub use self::error::{
     EvalError, EvalResult, EvalErrorKind, AssertMessage, ConstEvalErr, struct_error,
@@ -14,6 +25,29 @@ pub use self::error::{
 };
 
 pub use self::value::{Scalar, Value, ConstValue};
+
+pub use self::eval_context::{EvalContext, Frame, StackPopCleanup,
+                             TyAndPacked, ValTy};
+
+pub use self::place::{Place, PlaceExtra};
+
+pub use self::memory::{Memory, MemoryKind, HasMemory};
+
+pub use self::const_eval::{
+    eval_promoted,
+    mk_borrowck_eval_cx,
+    mk_eval_cx,
+    CompileTimeEvaluator,
+    const_value_to_allocation_provider,
+    const_eval_provider,
+    const_val_field,
+    const_variant_index,
+    value_to_const_value,
+};
+
+pub use self::machine::Machine;
+
+pub use self::memory::{write_target_uint, write_target_int, read_target_uint};
 
 use std::fmt;
 use mir;
@@ -35,6 +69,9 @@ use byteorder::{WriteBytesExt, ReadBytesExt, LittleEndian, BigEndian};
 use ty::codec::TyDecoder;
 use std::sync::atomic::{AtomicU32, Ordering};
 use std::num::NonZeroU32;
+
+use rustc::mir::interpret::{EvalResult, EvalErrorKind};
+use rustc::ty::{Ty, TyCtxt, ParamEnv};
 
 #[derive(Clone, Debug, PartialEq, Eq, Hash, RustcEncodable, RustcDecodable)]
 pub enum Lock {
@@ -677,4 +714,24 @@ fn bit_index(bits: Size) -> (usize, usize) {
     assert_eq!(a as usize as u64, a);
     assert_eq!(b as usize as u64, b);
     (a as usize, b as usize)
+}
+pub fn sign_extend<'a, 'tcx>(tcx: TyCtxt<'a, 'tcx, 'tcx>, value: u128, ty: Ty<'tcx>) -> EvalResult<'tcx, u128> {
+    let param_env = ParamEnv::empty();
+    let layout = tcx.layout_of(param_env.and(ty)).map_err(|layout| EvalErrorKind::Layout(layout))?;
+    let size = layout.size.bits();
+    assert!(layout.abi.is_signed());
+    // sign extend
+    let shift = 128 - size;
+    // shift the unsigned value to the left
+    // and back to the right as signed (essentially fills with FF on the left)
+    Ok((((value << shift) as i128) >> shift) as u128)
+}
+
+pub fn truncate<'a, 'tcx>(tcx: TyCtxt<'a, 'tcx, 'tcx>, value: u128, ty: Ty<'tcx>) -> EvalResult<'tcx, u128> {
+    let param_env = ParamEnv::empty();
+    let layout = tcx.layout_of(param_env.and(ty)).map_err(|layout| EvalErrorKind::Layout(layout))?;
+    let size = layout.size.bits();
+    let shift = 128 - size;
+    // truncate (shift left to drop out leftover values, shift right to fill with zeroes)
+    Ok((value << shift) >> shift)
 }
